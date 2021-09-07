53103f12e9c71c1ae367d371b8ccf0138d1968cc
99058830 <99058830@mydavinci.nl>
2021-09-07 12:07:49 +0200

    fix

--- feestlunch.py
+++ feestlunch.py
@@ -1,1 +1,5 @@
-88-2 * (27.98 + 15)
+Croissant = 0.39
+Stokbrood = 2.78
+Kortingsbon = 0.50
+
+print(17*Croissant + 2*Stokbrood - 3*Kortingsbon)
---
+++ speelhal.py
@@ -0,0 +1,5 @@
+Personen = 3
+Toegang = 7.45
+Vr = 0.37
+
+print(9*Vr + Toegang * Personen)
