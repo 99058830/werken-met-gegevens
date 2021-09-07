57b8efe6ccbd5807fafe6ee275354fa8c204687e
99058830 <99058830@mydavinci.nl>
2021-09-07 13:22:10 +0200

    latest

--- feestlunch.py
+++ feestlunch.py
@@ -1,5 +1,13 @@
 Croissant = 0.39
+hoeveelCroissant = 17
 Stokbrood = 2.78
+hoeveelStokbrood = 2
 Kortingsbon = 0.50
+hoeveelKortingsbon = 3
 
-print(17*Croissant + 2*Stokbrood - 3*Kortingsbon)
+if hoeveelKortingsbon >= 0.01: 
+    Prijs = hoeveelCroissant*Croissant + hoeveelStokbrood*Stokbrood - hoeveelKortingsbon*Kortingsbon
+    print("De feestlunch kost je bij de bakker", Prijs, "euro voor de", hoeveelCroissant,  "croissantjes en de", hoeveelStokbrood, "stokbroden als de", hoeveelKortingsbon, "kortingsbonnen nog geldig zijn!")
+else:
+    Prijs = hoeveelCroissant*Croissant + hoeveelStokbrood*Stokbrood
+    print("De feestlunch kost je bij de bakker", Prijs, "euro voor de", hoeveelCroissant,  "croissantjes en de", hoeveelStokbrood, "stokbroden!")
--- speelhal.py
+++ speelhal.py
@@ -2,4 +2,5 @@ Personen = 3
 Toegang = 7.45
 Vr = 0.37
 
-print(9*Vr + Toegang * Personen)
+Prijs = 9*Vr + Toegang * Personen
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar", Prijs, "euro")
